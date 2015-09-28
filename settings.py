b5179ca827301e420927afef862459910c813510
Wensheng Tang <sp3478@gmail.com>
2015-09-28 12:59:01 +0800

    Fix environ vars

--- settings.py
+++ settings.py
@@ -3,8 +3,9 @@
 
 from __future__ import unicode_literals,\
     absolute_import, division, print_function
+import os
 
-TWITTER_CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY')
-TWITTER_CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET')
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 DEBUG = True
 SECRET_KEY = str('')
